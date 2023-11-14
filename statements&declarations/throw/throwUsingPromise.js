61303c3ce47b3e5134d14130216206b69ff89f46
Elizbeh <eliz01_01@yahoo.com>
2023-11-14 15:34:25 +0100

    Throwing an existing object

---
+++ statements&declarations/throw/throwUsingPromise.js
@@ -0,0 +1,17 @@
+function readFilePromise(path) {
+  return new Promise((resolve, reject) => {
+    readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+try {
+  const data = await readFilePromise("foo.txt");
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
