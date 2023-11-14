33291c4ef84b1cda9b86a77d014427be2c625d86
Elizbeh <eliz01_01@yahoo.com>
2023-11-14 14:09:34 +0100

    Statement - Return

---
+++ statements&declarations/returnStatement.js
@@ -0,0 +1,10 @@
+//The return statement ends function execution and specifies a value to be returned to the function caller.
+function getRectArea(width, height){
+  if (width > 0 && height > 0){
+    return width * height;
+  }
+  return 0;
+}
+
+console.log(getRectArea(3, 4))
+console.log(getRectArea(-3, 4))
